6805e768058f32032a1f26c240906908e4443406
Fabio Catinella <f.catinella@zero12.it>
2024-08-19 10:49:23 +0200

    Added basic navbar

--- app/src/main/java/it/fabiocati/thegamedb/ui/main/TheGameDbApp.kt
+++ app/src/main/java/it/fabiocati/thegamedb/ui/main/TheGameDbApp.kt
@@ -1,26 +1,44 @@
 package it.fabiocati.thegamedb.ui.main
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.generated.NavGraphs
 
 @Composable
 fun TheGameDbApp() {
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        DestinationsNavHost(navGraph = NavGraphs.root)
+        DestinationsNavHost(
+            navGraph = NavGraphs.root,
+            modifier = Modifier.weight(1f)
+        )
+        NavigationBar {
+            NavigationBarItem(
+                selected = true,
+                onClick = {},
+                icon = { Icon(Icons.Filled.Home, contentDescription = "Home") }
+            )
+            NavigationBarItem(
+                selected = false,
+                onClick = { },
+                icon = { Icon(Icons.Filled.Search, contentDescription = "Search") }
+            )
+            NavigationBarItem(
+                selected = false,
+                onClick = { },
+                icon = { Icon(Icons.Filled.Menu, contentDescription = "Menu") }
+            )
+        }
     }
-}
-
-
-@Preview(device = Devices.PIXEL_5)
-@Composable
-private fun TheGameDbAppPreview() {
-    TheGameDbApp()
 }
