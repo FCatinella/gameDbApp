6215626abf58e00c31b94fa388e029b1a9c1b518
Fabio Catinella <f.catinella@zero12.it>
2025-01-02 16:48:04 +0100

    Added Glance widget dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
 
     implementation(libs.kotlinx.datetime)
 
+    implementation(libs.bundles.androidx.glance.app.widget)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
