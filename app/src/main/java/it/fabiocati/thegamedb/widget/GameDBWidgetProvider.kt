b2278b9235de168e63c887249c7d7d59230d611e
Fabio Catinella <f.catinella@zero12.it>
2025-01-25 17:00:12 +0100

    Added declaration on GameDBWidgetProvider

---
+++ app/src/main/java/it/fabiocati/thegamedb/widget/GameDBWidgetProvider.kt
@@ -0,0 +1,9 @@
+package it.fabiocati.thegamedb.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class GameDBWidgetProvider : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget
+        get() = TODO("Not yet implemented")
+}
