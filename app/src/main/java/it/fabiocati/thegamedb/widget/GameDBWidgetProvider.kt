510fe9cf79f2545a0f064915bf40d8e1d5160e03
Fabio Catinella <f.catinella@zero12.it>
2025-01-25 18:41:41 +0100

    First "hello world" widget

--- app/src/main/java/it/fabiocati/thegamedb/widget/GameDBWidgetProvider.kt
+++ app/src/main/java/it/fabiocati/thegamedb/widget/GameDBWidgetProvider.kt
@@ -1,9 +1,31 @@
 package it.fabiocati.thegamedb.widget
 
+import android.content.Context
+import androidx.compose.ui.graphics.Color
+import androidx.glance.GlanceId
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Box
+import androidx.glance.GlanceModifier
+import androidx.glance.background
+import androidx.glance.text.Text
 
 class GameDBWidgetProvider : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget
-        get() = TODO("Not yet implemented")
+        get() = GameDBWidget()
+}
+
+private class GameDBWidget : GlanceAppWidget() {
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+            Box(
+                modifier = GlanceModifier.background(Color.LightGray)
+            ) {
+                Text(
+                    text = "Hello World"
+                )
+            }
+        }
+    }
 }
