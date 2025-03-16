83e24c05f5f605bd3a829718babbfada30e0bf23
Fabio Catinella <Fabio.catinella@gmail.com>
2025-03-16 11:43:13 +0100

    Enhance Random Game Widget and add deep link navigation
    - Added deep link navigation to the Random Game Widget, allowing it to open the app and navigate to the game details screen.

--- app/src/main/java/it/fabiocati/thegamedb/widget/randomGame/RandomGameWidgetProvider.kt
+++ app/src/main/java/it/fabiocati/thegamedb/widget/randomGame/RandomGameWidgetProvider.kt
@@ -1,12 +1,18 @@
 package it.fabiocati.thegamedb.widget.randomGame
 
 import android.content.Context
+import android.content.Intent
+import androidx.core.net.toUri
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
 import androidx.glance.LocalSize
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.SizeMode
+import androidx.glance.appwidget.action.actionStartActivity
 import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Box
 import it.fabiocati.thegamedb.domain.model.PopularityType
 import it.fabiocati.thegamedb.domain.usecase.GetGameDetailsUseCase
 import it.fabiocati.thegamedb.domain.usecase.GetPopularGamesUseCase
@@ -43,11 +49,16 @@ private class RandomGameWidget : GlanceAppWidget() {
             koin.close()
             result
         }.onSuccess { game ->
+            val deepLinkIntent = buildOpenAppIntent(game.id.toInt())
             provideContent {
-                when (LocalSize.current) {
-                    RandomGameWidgetSize.COMPACT -> RandomGameWidgetContentCompact(game)
-                    RandomGameWidgetSize.MEDIUM -> RandomGameWidgetContentMedium(game)
-                    RandomGameWidgetSize.EXPANDED -> RandomGameWidgetContentExpanded(game)
+                Box(
+                    modifier = GlanceModifier.clickable(actionStartActivity(deepLinkIntent))
+                ) {
+                    when (LocalSize.current) {
+                        RandomGameWidgetSize.COMPACT -> RandomGameWidgetContentCompact(game)
+                        RandomGameWidgetSize.MEDIUM -> RandomGameWidgetContentMedium(game)
+                        RandomGameWidgetSize.EXPANDED -> RandomGameWidgetContentExpanded(game)
+                    }
                 }
             }
         }.onFailure {
@@ -56,4 +67,9 @@ private class RandomGameWidget : GlanceAppWidget() {
             }
         }
     }
+
+    private fun buildOpenAppIntent(gameId: Int) =
+        Intent(Intent.ACTION_VIEW).apply {
+            data = "app://gamedb.fabiocati.it/$gameId".toUri()
+        }
 }
